4ae36e38b427fba5b6305ecda4f22fc1c97fa0c7
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-05-22 15:00:35 +0600

    forms plugin install

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useCallback } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import Input from '~/app/components/inputs/Input';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -17,7 +18,7 @@ const AuthForm = () => {
     }
   }, [variant]);
 
-  const {} = useForm<FieldValues>({
+  const { register, handleSubmit } = useForm<FieldValues>({
     defaultValues: {
       name: '',
       email: '',
@@ -27,9 +28,31 @@ const AuthForm = () => {
 
   const onSubmit: SubmitHandler<FieldValues> = data => {
     setIsLoading(true);
+
+    if (variant === 'LOGIN') {
+      // nextAuth
+    }
+
+    if (variant === 'REGISTER') {
+      // axios
+    }
   };
 
-  return <div>Hi</div>;
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    // NextAuth Social Sign In
+  };
+
+  return (
+    <div className=' mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+      <div className=' bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
+        <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
+          <Input label='Email' />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
