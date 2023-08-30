6b8aedbd11f6ed3b88a99aa0f92ac3c40ae40717
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-08-30 16:47:43 +0600

    ➖ removed everything

--- app/actions/getSession.ts
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from "next-auth"
-import { authOption } from "~/app/api/auth/[...nextauth]/route"
-
-const getSession = async () => {
-  return await getServerSession(authOption)
-}
-
-export default getSession
